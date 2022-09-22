ca36faebf91030d63ef05142ea289138f3de6cf6
MD MARUF BILLAH <110770179+maruf4253@users.noreply.github.com>
2022-09-22 17:16:35 +0600

    restaureantsite

---
+++ JS/main.js
@@ -0,0 +1,15 @@
+$('.owl-carousel, .loop').owlCarousel({
+    center: true,
+    items: 2,
+    loop: true,
+    margin: 10,
+    dots: true,
+    nav: false,
+    responsive: true,
+    responsive:{
+        600:{
+            items:3
+        }
+    }
+}) 
+
